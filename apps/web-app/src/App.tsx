44c2bd7f494c4d038ed13c2bc91a78db09534910
bronifty <bronifty@gmail.com>
2024-05-21 19:56:56 -0500

    this

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,12 +1,21 @@
 import { isBlank } from "common";
+import { add, sub } from "ts-lib";
 
 const App = () => {
   return (
     <>
-      <p>undefined isBlank - {isBlank(undefined) ? "true" : "false"}</p>
-      <p>false isBlank - {isBlank(false) ? "true" : "false"}</p>
-      <p>true isBlank - {isBlank(true) ? "true" : "false"}</p>
-      <p>Empty object isBlank - {isBlank({}) ? "true" : "false"}</p>
+      <section>
+        <h1>test 1 - common lib</h1>
+        <p>undefined isBlank - {isBlank(undefined) ? "true" : "false"}</p>
+        <p>false isBlank - {isBlank(false) ? "true" : "false"}</p>
+        <p>true isBlank - {isBlank(true) ? "true" : "false"}</p>
+        <p>Empty object isBlank - {isBlank({}) ? "true" : "false"}</p>
+      </section>
+      <section>
+        <h1>test 2 - ts-lib</h1>
+        <p>add(1,2) - {add(1, 2)}</p>
+        <p>sub(1,2) - {sub(1, 2)}</p>
+      </section>
     </>
   );
 };
