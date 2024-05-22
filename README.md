04cd63db11ee6d11f31ac28bdb38269d4c0f9dc9
bronifty <bronifty@gmail.com>
2024-05-22 07:11:34 -0500

    this

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-#PNPM monorepo
+#PNPM monorepo for marcs-observable
+
+### Setup
+
+- web-app is to test the functionality of the package
+- web-app has a dependency on marcs-observable package
+- install everything from the root, then build marcs-observable, update everything from the root to push the changes to the web-app and finally run it and also test (q to quit because test watches)
+- cleanup
 
 ```sh
-pnpm i && pnpm update
+pnpm i
+pnpm build
+pnpm update
+pnpm dev
 pnpm test
+pnpm clean
 ```
--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { isBlank } from "common";
-import { add, sub } from "ts-lib";
 import { Observable, ObservableFactory } from "marcs-observable";
 
 // creating observables outside of the component in order to preserve state when component re-renders
@@ -23,35 +21,6 @@ const App = () => {
 
   return (
     <>
-      <section>
-        <h1>test 1 - common lib</h1>
-        <p>undefined isBlank - {isBlank(undefined) ? "true" : "false"}</p>
-        <p>false isBlank - {isBlank(false) ? "true" : "false"}</p>
-        <p>true isBlank - {isBlank(true) ? "true" : "false"}</p>
-        <p>Empty object isBlank - {isBlank({}) ? "true" : "false"}</p>
-      </section>
-      <section>
-        <h1>test 2 - ts-lib</h1>
-        <p>add(1,2) - {add(1, 2)}</p>
-        <p>sub(1,2) - {sub(1, 2)}</p>
-      </section>
-      <section>
-        <input
-          id="input1"
-          type="number"
-          value={input1}
-          onChange={(e) => setInput1(parseInt(e.target.value))}
-        />
-        <input
-          id="input2"
-          type="number"
-          value={input2}
-          onChange={(e) => setInput2(parseInt(e.target.value))}
-        />
-
-        <p>add {add(input1, input2)}</p>
-        <p>sub {sub(input1, input2)}</p>
-      </section>
       <section>
         <h1>test 3 - marcs-observable</h1>
         <p>child.value - {child.value}</p>
