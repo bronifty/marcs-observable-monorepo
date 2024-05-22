2e9ae887e05ba2d76a8d6ebe6d6b3bb29f44b47f
bronifty <bronifty@gmail.com>
2024-05-21 20:09:04 -0500

    this

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { isBlank } from "common";
 import { add, sub } from "ts-lib";
 
 const App = () => {
+  const [input1, setInput1] = React.useState(4);
+  const [input2, setInput2] = React.useState(3);
   return (
     <>
       <section>
@@ -15,6 +18,23 @@ const App = () => {
         <h1>test 2 - ts-lib</h1>
         <p>add(1,2) - {add(1, 2)}</p>
         <p>sub(1,2) - {sub(1, 2)}</p>
+      </section>
+      <section>
+        <input
+          id="input1"
+          type="number"
+          value={input1}
+          onChange={(e) => setInput1(parseInt(e.target.value))}
+        />
+        <input
+          id="input2"
+          type="number"
+          value={input2}
+          onChange={(e) => setInput2(parseInt(e.target.value))}
+        />
+
+        <p>add {add(input1, input2)}</p>
+        <p>sub {sub(input1, input2)}</p>
       </section>
     </>
   );
